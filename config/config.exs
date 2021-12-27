67da435cec2f7f23e612291f52aa114707e436d4
woojiahao <woojiahao1234@gmail.com>
2021-12-27 20:28:52 +0800

    Add basic grid coloring

--- config/config.exs
+++ config/config.exs
@@ -5,7 +5,7 @@ use Mix.Config
 # Configure the main viewport for the Scenic application
 config :life, :viewport, %{
   name: :main_viewport,
-  size: {700, 600},
+  size: {1200, 800},
   default_scene: {Life.Scene.Home, nil},
   drivers: [
     %{
--- lib/scenes/home.ex
+++ lib/scenes/home.ex
@@ -6,33 +6,83 @@ defmodule Life.Scene.Home do
   alias Scenic.ViewPort
 
   import Scenic.Primitives
-  @text_size 24
+  import Scenic.Components
 
-  defp generate_grid(graph, width, height, cell_size \\ 50) do
-    Enum.reduce(0..width//cell_size, graph, fn x, acc ->
-      line(acc, {{x, 0}, {x, height}}, stroke: {2, :white})
-    end)
-    |> then(fn g ->
-      Enum.reduce(0..height//cell_size, g, fn y, acc ->
-        line(acc, {{0, y}, {width, y}}, stroke: {2, :white})
-      end)
+  @text_size 24
+  @cell_size 50
+
+  defp generate_grid(graph, width, height) do
+    # TODO: Fix bug where we are generating an entirely new row and column
+    cells = for row <- 0..width//@cell_size, col <- 0..height//@cell_size, do: {row, col}
+
+    Enum.reduce(cells, graph, fn {row, col}, acc ->
+      id = String.to_atom("#{row}:#{col}")
+
+      rectangle(acc, {@cell_size, @cell_size},
+        translate: {row, col},
+        stroke: {2, :white},
+        id: id
+      )
     end)
   end
 
+  @impl Scenic.Scene
   def init(_, opts) do
-    # get the width and height of the viewport. This is to demonstrate creating
-    # a transparent full-screen rectangle to catch user input
-    {:ok, %ViewPort.Status{size: {width, height}}} = ViewPort.info(opts[:viewport])
+    {:ok, %ViewPort.Status{size: {_, height}}} = ViewPort.info(opts[:viewport])
+
+    # Grid size will be the total height of the viewport
+    grid_size = height
+    x_offset = grid_size + 100
+
+    # Grid state will be a matrix represented as a dictionary of the given grid size with a boolean state denoting if
+    # it is on/off
+    cells = for row <- 0..height//@cell_size, col <- 0..height//@cell_size, do: {row, col}
+    grid_state = cells |> Map.new(fn pos -> {pos, false} end)
 
     graph =
       Graph.build(font: :roboto, font_size: @text_size)
-      |> generate_grid(width, height)
+      |> generate_grid(grid_size, grid_size)
+      |> text("Life Iterations: ", translate: {x_offset, 100}, id: :life_iterations)
+      |> button("Start", translate: {x_offset, 150}, id: :pause)
 
-    {:ok, graph, push: graph}
+    state = %{
+      graph: graph,
+      grid_state: grid_state,
+      is_started?: false,
+      iteration: 0
+    }
+
+    {:ok, state, push: graph}
   end
 
-  def handle_input(event, _context, state) do
-    Logger.info("Received event: #{inspect(event)}")
-    {:noreply, state}
+  @impl Scenic.Scene
+  def filter_event(
+        {:click, :pause} = event,
+        _from,
+        %{
+          graph: graph,
+          grid_state: grid_state,
+          is_started?: is_started?
+        } = state
+      ) do
+    IO.inspect("Button clicked, board state is #{is_started?}")
+
+    updated_grid_state = grid_state |> Map.new(fn {pos, v} -> {pos, !v} end)
+
+    updated_graph =
+      updated_grid_state
+      |> Enum.reduce(graph, fn {{row, col}, v}, acc ->
+        id = String.to_atom("#{row}:#{col}")
+        fill = if v, do: :blue, else: :clear
+        acc |> Graph.modify(id, &rectangle(&1, {@cell_size, @cell_size}, fill: fill))
+      end)
+
+    {:cont, event,
+     %{
+       state
+       | graph: updated_graph,
+         is_started?: !is_started?,
+         grid_state: updated_grid_state
+     }, push: updated_graph}
   end
 end
