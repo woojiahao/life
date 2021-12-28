9c6f6e57ca1b22ceb59686d2de8ea57b1bfd3ae1
woojiahao <woojiahao1234@gmail.com>
2021-12-28 19:25:19 +0800

    Move graph building to compile time

--- lib/scenes/home.ex
+++ lib/scenes/home.ex
@@ -10,49 +10,45 @@ defmodule Life.Scene.Home do
 
   @text_size 24
   @cell_size 50
+  @grid_size 800
+  @graph Graph.build(font: :roboto, font_size: @text_size)
+         |> then(fn g ->
+           # TODO: Fix bug where we are generating an entirely new row and column
+           cells =
+             for row <- 0..@grid_size//@cell_size,
+                 col <- 0..@grid_size//@cell_size,
+                 do: {row, col}
 
-  defp generate_grid(graph, width, height) do
-    # TODO: Fix bug where we are generating an entirely new row and column
-    cells = for row <- 0..width//@cell_size, col <- 0..height//@cell_size, do: {row, col}
+           Enum.reduce(cells, g, fn {row, col}, acc ->
+             id = String.to_atom("#{row}:#{col}")
 
-    Enum.reduce(cells, graph, fn {row, col}, acc ->
-      id = String.to_atom("#{row}:#{col}")
-
-      rectangle(acc, {@cell_size, @cell_size},
-        translate: {row, col},
-        stroke: {2, :white},
-        id: id
-      )
-    end)
-  end
+             rectangle(acc, {@cell_size, @cell_size},
+               translate: {row, col},
+               stroke: {2, :white},
+               id: id
+             )
+           end)
+         end)
+         |> text("Life Iterations: ", translate: {@grid_size + 100, 100}, id: :life_iteration)
+         |> button("Start", translate: {@grid_size + 100, 150}, id: :pause)
 
   @impl Scenic.Scene
   def init(_, opts) do
     {:ok, %ViewPort.Status{size: {_, height}}} = ViewPort.info(opts[:viewport])
-
-    # Grid size will be the total height of the viewport
-    grid_size = height
-    x_offset = grid_size + 100
 
     # Grid state will be a matrix represented as a dictionary of the given grid size with a boolean state denoting if
     # it is on/off
     cells = for row <- 0..height//@cell_size, col <- 0..height//@cell_size, do: {row, col}
     grid_state = cells |> Map.new(fn pos -> {pos, false} end)
 
-    graph =
-      Graph.build(font: :roboto, font_size: @text_size)
-      |> generate_grid(grid_size, grid_size)
-      |> text("Life Iterations: ", translate: {x_offset, 100}, id: :life_iterations)
-      |> button("Start", translate: {x_offset, 150}, id: :pause)
-
     state = %{
-      graph: graph,
+      graph: @graph,
       grid_state: grid_state,
       is_started?: false,
       iteration: 0
     }
 
-    {:ok, state, push: graph}
+    {:ok, state, push: @graph}
   end
 
   @impl Scenic.Scene
